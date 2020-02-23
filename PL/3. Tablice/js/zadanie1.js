8414c57041280a2b059c39ce582394c76df8b167
szczepaniakmichal <mssk87@gmail.com>
2020-02-23 12:43:17 +0100

    Array

---
+++ PL/3. Tablice/js/zadanie1.js
@@ -0,0 +1,9 @@
+const friendsName = ['Adrian', 'Kasia', 'Anastazja', 'Krzysztof', 'Piotr'];
+
+console.log(`Liczba moich znajomych to ${friendsName.length}`);
+
+const firstFriend = friendsName[0].valueOf();
+console.log(firstFriend);
+
+const lastFriend = friendsName[friendsName.length - 1].valueOf();
+console.log(lastFriend);
