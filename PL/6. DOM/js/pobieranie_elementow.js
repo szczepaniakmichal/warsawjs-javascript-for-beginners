8374b864443517b8d34116bcfb0edf4cae74de92
szczepaniakmichal <mssk87@gmail.com>
2020-02-23 16:31:17 +0100

    DOM

---
+++ PL/6. DOM/js/pobieranie_elementow.js
@@ -0,0 +1,87 @@
+const title = document.querySelector("#mojnaglowek");
+title.textContent = 'Javascript jest super';
+
+const input = document.querySelector("#coTutajJestNapisane");
+console.log(input);
+
+input.value = ' to nowa tresc';
+
+title.textContent = input.value;
+
+const newEl = document.createElement("p");
+
+newEl.textContent = 'createElement';
+
+
+const body = document.querySelector('body');
+body.appendChild(newEl);
+
+let comment = {
+    author: "Andrzej Molek",
+    date: "20.03.2019",
+    msg: "Ale super ten warsztat robimy już czary mary"
+}
+
+
+const idComments = document.querySelector("#comments");
+const author = document.createElement("h2");
+author.textContent = comment.author;
+
+idComments.appendChild(author);
+
+const spanDate = document.createElement('span');
+spanDate.textContent = comment.date;
+idComments.appendChild(spanDate);
+
+const paragrafMsg = document.createElement("p");
+paragrafMsg.textContent = comment.msg;
+idComments.appendChild(paragrafMsg);
+
+
+
+let comments = [
+    {
+        author: "Wojtek Trek",
+        date: "20.03.2019",
+        msg: "Już taki jestem..."
+    },
+    {
+        author: "Strach na wróble",
+        date: "21.03.2019",
+        msg: "Zimny drań"
+    },
+    {
+        author: "Wokay",
+        date: "24.03.2019",
+        msg: "I dobrze mi z tym"
+    },
+    {
+        author: "Zenek Martyniuk",
+        date: "26.03.2019",
+        msg: "Miłość miłość w zakopanym"
+    },
+    {
+        author: "Sławomir",
+        date: "30.03.2019",
+        msg: "Ej Zenek, ukradłeś mi tekst..."
+    }
+]
+
+comments.forEach(el => {
+
+    const author = document.createElement("h2");
+    author.textContent = el.author;
+
+    idComments.appendChild(author);
+
+    const spanDate = document.createElement('span');
+    spanDate.textContent = el.date;
+    idComments.appendChild(spanDate);
+
+    const paragrafMsg = document.createElement("p");
+    paragrafMsg.textContent = el.msg;
+    idComments.appendChild(paragrafMsg);
+});
+
+
+
